350c54ceceee65825cd150ead82e3c9092098b1b
Lucas van der Have <lucasvdhave@gmail.com>
2025-01-03 21:14:36 +0100

    feat: downgrade v2 phpunit

--- src/FinderCommand.php
+++ src/FinderCommand.php
@@ -3,8 +3,8 @@
 namespace PhpUnitFinder;
 
 use PHPUnit\Framework\TestCase;
-use PHPUnit\TextUI\TestSuiteMapper;
-use PHPUnit\TextUI\XmlConfiguration\Loader;
+use PHPUnit\Framework\TestSuite;
+use PHPUnit\Util\Configuration;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -12,49 +12,83 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
- * A symfony command for finding PHPUnit files.
+ * A symfony command for finding PHPUnit files (compatible with PHPUnit 8.x).
  */
-class FinderCommand extends Command {
+class FinderCommand extends Command
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->addOption(
+            'config-file',
+            'c',
+            InputOption::VALUE_OPTIONAL,
+            "The phpunit.xml config file to use.",
+            getcwd() . '/phpunit.xml'
+        );
+        $this->addOption(
+            'bootstrap-file',
+            'b',
+            InputOption::VALUE_OPTIONAL,
+            "The tests bootstrap file.",
+            getcwd() . '/tests/bootstrap.php'
+        );
+        $this->addArgument(
+            'test-suite',
+            InputArgument::IS_ARRAY,
+            "The test suites to scan."
+        );
+    }
 
-  /**
-   * {@inheritdoc}
-   */
-  protected function configure() {
-    $this->addOption('config-file', 'c', InputOption::VALUE_OPTIONAL, "The phpunit.xml config file to use.", getcwd() . '/phpunit.xml');
-    $this->addOption('bootstrap-file', 'b', InputOption::VALUE_OPTIONAL, "The tests bootstrap file.", getcwd() . '/tests/bootstrap.php');
-    $this->addArgument('test-suite', InputArgument::IS_ARRAY, "The test suites to scan.");
-  }
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // Read CLI options / arguments
+        $configFile = $input->getOption('config-file');
+        $bootstrap  = $input->getOption('bootstrap-file');
+        $testSuites = $input->getArgument('test-suite');
 
-  /**
-   * {@inheritdoc}
-   */
-  protected function execute(InputInterface $input, OutputInterface $output) {
-    $configFile = $input->getOption('config-file');
-    $bootstrap = $input->getOption('bootstrap-file');
-    include_once $bootstrap;
-    $testSuites = $input->getArgument('test-suite');
-    $testFilenames = [];
+        // Include your bootstrap if needed
+        include_once $bootstrap;
 
-    $config = (new Loader())->load($configFile);
+        // Load the PHPUnit XML configuration
+        $config    = Configuration::getInstance($configFile);
+        $mainSuite = $config->getTestSuiteConfiguration();
 
-    foreach ($config->testSuite() as $suite) {
-      if ($testSuites && !in_array($suite->name(), $testSuites, TRUE)) {
-        continue;
-      }
-      $testSuite = (new TestSuiteMapper)->map($config->testSuite(), $suite->name());
-      foreach (new \RecursiveIteratorIterator($testSuite) as $test) {
-        if ($test instanceof TestCase) {
-          $testFilenames[] = ((new \ReflectionClass($test))->getFileName());
+        $testFilenames = [];
+
+        // Loop through sub-suites in the main config
+        foreach ($mainSuite->tests() as $suite) {
+            // Only proceed if this is a proper TestSuite
+            if (!$suite instanceof TestSuite) {
+                continue;
+            }
+
+            // If specific suite names were given, skip others
+            if (!empty($testSuites) && !in_array($suite->getName(), $testSuites, true)) {
+                continue;
+            }
+
+            // Recursively walk through all tests in this suite
+            foreach (new \RecursiveIteratorIterator($suite) as $test) {
+                if ($test instanceof TestCase) {
+                    $reflection     = new \ReflectionClass($test);
+                    $testFilenames[] = $reflection->getFileName();
+                }
+            }
         }
-      }
+
+        // Print unique files
+        $testFilenames = array_unique($testFilenames);
+        foreach ($testFilenames as $file) {
+            $output->writeln($file);
+        }
+
+        // Return 0 to indicate success
+        return 0;
     }
-
-    $testFilenames = array_unique($testFilenames);
-    foreach ($testFilenames as $testFilename) {
-      $output->writeln($testFilename);
-    }
-
-    return 0;
-  }
-
 }
